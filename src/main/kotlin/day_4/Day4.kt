17280f47c9829e85da15ca489f7eea8fa8335930
Piotr Gazda <pjgazda@interia.pl>
2023-12-14 00:18:27 +0100

    Solution for day four challenge - part one, code refactor.

--- src/main/kotlin/day_4/Day4.kt
+++ src/main/kotlin/day_4/Day4.kt
@@ -7,19 +7,22 @@ import kotlin.math.pow
 val DAY2_FILE_NAME = "day_4\\input.txt"
 fun main() {
     val path = Paths.get("").toAbsolutePath().toString() + "\\src\\main\\kotlin\\$DAY2_FILE_NAME"
-    read_file(path)
+    readFile(path)
 }
 
 
-fun read_file(filename:String) {
-    val res = File(filename).bufferedReader().readLines().map{ it ->
+fun readFile(filename:String) {
+    val result = File(filename).bufferedReader().readLines().sumOf { it ->
         val (winning, players) = it.split(": ")[1].split(" | ")
-        val intersect = winning.split(" ").filter{it != ""}.intersect(players.split(" ").filter{it != ""}.toSet())
-        var result = 0.0
-        if(intersect.isNotEmpty()) {
-           result = 2.0.pow(intersect.count() - 1)
+        val intersect = winning.split(" ")
+            .filter { it.isNotEmpty() }.intersect(players.split(" ")
+                .filter { it.isNotEmpty() }.toSet()
+            )
+        if (intersect.isNotEmpty()) {
+            2.0.pow(intersect.size - 1)
+        } else {
+            0.0
         }
-        result
-    }.sum()
-    println(res)
+    }
+    println(result)
 }
