1066bb20dcb54cd08dd25bbe18c4b57a47075e9a
Piotr Gazda <pjgazda@interia.pl>
2023-12-14 11:41:57 +0100

    Solution for day four challenge - part two.

--- src/main/kotlin/day_4/Day4.kt
+++ src/main/kotlin/day_4/Day4.kt
@@ -11,18 +11,35 @@ fun main() {
 }
 
 
-fun readFile(filename:String) {
-    val result = File(filename).bufferedReader().readLines().sumOf { it ->
+fun readFile(filename: String) {
+    val lines = File(filename).bufferedReader().readLines()
+    val cards = lines.map { it ->
         val (winning, players) = it.split(": ")[1].split(" | ")
-        val intersect = winning.split(" ")
-            .filter { it.isNotEmpty() }.intersect(players.split(" ")
-                .filter { it.isNotEmpty() }.toSet()
-            )
-        if (intersect.isNotEmpty()) {
-            2.0.pow(intersect.size - 1)
+        winning.split(" ")
+            .filter { it.isNotEmpty() }.intersect(
+                players.split(" ")
+                    .filter { it.isNotEmpty() }.toSet()
+            ).count()
+    }.toList()
+    val result = cards.sumOf {
+        if (it != 0) {
+            2.0.pow(it - 1)
         } else {
             0.0
         }
     }
     println(result)
+
+    val wonCards = mutableMapOf<Int, Int>().apply {
+        for (i in cards.indices) this[i] = 1
+    }
+
+    cards.indices.forEach { i ->
+        (1..wonCards[i]!!).forEach {
+            (1..cards[i]).forEach { j ->
+                wonCards[i + j] = wonCards[i + j]?.plus(1) ?: 0
+            }
+        }
+    }
+    println(wonCards.values.sum())
 }
