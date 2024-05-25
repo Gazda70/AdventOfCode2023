08a34c5d3588015a4565ec8c31840ce3e73247b7
Piotr Gazda <pjgazda@interia.pl>
2024-05-25 15:39:50 +0200

    Solution for day six challenge - part two.

--- src/main/kotlin/day_6/Day6.kt
+++ src/main/kotlin/day_6/Day6.kt
@@ -13,8 +13,14 @@ fun main() {
 fun readFile(filename: String) {
     val lines = File(filename).readLines()
     val regex = "\\s+".toRegex()
-    val timesDistances = (lines[0].split(": ")[1].split(regex) zip
-            lines[1].split(": ")[1].split(regex)).drop(1).map { timesDistance ->
+    val times = lines[0].split(": ")[1].split(regex).drop(1)
+    val distances = lines[1].split(": ")[1].split(regex).drop(1)
+    partFirstSolution(times, distances)
+    partTwoSolution(times, distances)
+}
+
+fun partFirstSolution(times: List<String>, distances: List<String>) {
+    val timesDistances = (times zip distances).map { timesDistance ->
         Pair(timesDistance.first.toInt(), timesDistance.second.toInt())
     }
     var totalCounter = 1
@@ -28,4 +34,16 @@ fun readFile(filename: String) {
         totalCounter = totalCounter.times(counter)
     }
     println(totalCounter)
+}
+
+fun partTwoSolution(times: List<String>, distances: List<String>) {
+    val time= times.joinToString("").toLong()
+    val distance = distances.joinToString("").toLong()
+    var counter = 0
+    for (speed in 0..time) {
+        if (speed.times(time - speed) > distance) {
+            counter++
+        }
+    }
+    println(counter)
 }
